7dbed53c9e5476c7cf4c4673710822d182ffbb6f
Alex Gilbert <alexgilbert@yahoo.com>
2023-03-05 12:05:20 -0500

    SOMETHING FUCKING WORKS AND IM SO HAPPY

---
+++ DLCQuestipelago/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.NetLauncher;
+using BepInEx.NetLauncher.Common;
+using HarmonyLib;
+
+namespace DLCQuestipelago
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        private Harmony _harmony;
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            _harmony = new Harmony(PluginInfo.PLUGIN_NAME);
+            _harmony.PatchAll();
+
+            DLCPackPatch.Initialize(Log);
+        }
+    }
+}
